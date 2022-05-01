280f0bcfb60887b1664beab7573bc445c32a1814
NightiX <waegemans.q@gmail.com>
2022-05-01 16:13:27 +0200

    Start learning

---
+++ src/main/java/com/learning/HRAssociationApi/HrAssociationApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.HRAssociationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAssociationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAssociationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/HRAssociationApi/HrAssociationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.HRAssociationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAssociationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
